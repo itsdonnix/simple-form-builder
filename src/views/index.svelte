7c20209715780ba5a54508baced67fdca729c35b
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-13 15:46:49 +0800

    Add title to create new form button
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/views/index.svelte
+++ src/views/index.svelte
@@ -6,7 +6,10 @@
 <div class="mt-4">
   <h3 class="p-2 font-bold h3">Baru</h3>
   <div class="m-3">
-    <a class="flex flex-col new-form-card--wrapper" href="/form/create">
+    <a
+      class="flex flex-col new-form-card--wrapper"
+      href="/form/create"
+      title="Buat formulir baru">
       <div class="flex items-center justify-center new-form-card">
         <Icon path={mdiPlusCircle} />
       </div>
