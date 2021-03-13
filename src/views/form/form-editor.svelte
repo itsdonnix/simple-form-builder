b72e94ca2effe57019b1ee7341f60941d747c520
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-13 21:01:13 +0800

    Add toolbar Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

---
+++ src/views/form/form-editor.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    document.body.classList.add('bg-color-2');
+    return () => document.body.classList.remove('bg-color-2');
+  });
+</script>
+
+<div class="p-2 bg-white form-actions">
+  <div class="flex margin-page">
+    <button class="px-4 py-2 ml-auto font-bold btn btn--primary">
+      Kirim
+    </button>
+  </div>
+</div>
