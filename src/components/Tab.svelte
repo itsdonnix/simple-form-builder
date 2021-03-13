ab658cacba9d6cd8546bf9a79f406d2fae41144b
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-13 23:22:24 +0800

    Add Tab components
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

---
+++ src/components/Tab.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { getContext } from 'svelte';
+  import { TABS } from './Tabs.svelte';
+
+  const tab = {};
+  const { registerTab, selectTab, selectedTab } = getContext(TABS);
+
+  registerTab(tab);
+</script>
+
+<button class:selected={$selectedTab === tab} on:click={() => selectTab(tab)}>
+  <slot />
+</button>
+
+<style>
+  button {
+    background: #f9f1f1;
+    border: none;
+    border-radius: 0;
+    margin: 0;
+    color: #585f66;
+    text-align: center;
+    width: 100%;
+    padding: 0.8em;
+  }
+
+  .selected {
+    color: #000;
+    font-weight: bold;
+    background-color: #fff;
+    positions: relative;
+  }
+</style>
---
+++ src/components/TabList.svelte
@@ -0,0 +1,9 @@
+<div class="tab-list">
+  <slot />
+</div>
+
+<style>
+  .tab-list {
+    display: flex;
+  }
+</style>
