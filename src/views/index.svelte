43f0cb692f0921ac3ebc78ca6a6918e6a53c346f
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-13 15:42:59 +0800

    Add main page
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/views/index.svelte
+++ src/views/index.svelte
@@ -1,3 +1,16 @@
 <script>
-  //
+  import { mdiPlusCircle } from '@mdi/js';
+  import Icon from '../components/Icon.svelte';
 </script>
+
+<div class="mt-4">
+  <h3 class="p-2 font-bold h3">Baru</h3>
+  <div class="m-3">
+    <a class="flex flex-col new-form-card--wrapper" href="/form/create">
+      <div class="flex items-center justify-center new-form-card">
+        <Icon path={mdiPlusCircle} />
+      </div>
+      <div class="my-2 text-center">Formulir Baru</div>
+    </a>
+  </div>
+</div>
