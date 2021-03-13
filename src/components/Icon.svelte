aac9c8b68d3e658c4fd528ff33a9d6f7ef1165a1
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-13 15:26:30 +0800

    Add Icon component
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

---
+++ src/components/Icon.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let size = '24px';
+  export let width = size;
+  export let height = size;
+  export let viewWidth = '24';
+  export let viewHeight = '24';
+  export let path = null;
+  export let label = null;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  {width}
+  {height}
+  viewBox="0 0 {viewWidth} {viewHeight}">
+  <path d={path}>
+    {#if label}
+      <title>{label}</title>
+    {/if}
+  </path>
+</svg>
