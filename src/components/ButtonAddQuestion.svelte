94ed513fcee8635ad1c5946515e6c7415cadec8b
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-16 05:11:56 +0800

    Improve add question button
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/components/ButtonAddQuestion.svelte
+++ src/components/ButtonAddQuestion.svelte
@@ -4,41 +4,38 @@
   import Icon from './Icon.svelte';
 
   let self;
-  let active = false;
 
   const emit = createEventDispatcher();
 
-  async function emitQuestionType(type) {
+  function emitQuestionType(type) {
     emit('onSelectQuestionType', type);
-  }
-
-  $: if (active) {
-    if (self) {
-      const button = self.querySelector('button');
-      button && button.focus();
-    }
   }
 </script>
 
-<button
-  bind:this={self}
-  class="flex items-center px-2 py-1 btn btn--primary"
-  on:click={() => (active = true)}
-  on:focusout={(e) => (active = self.contains(e.relatedTarget))}>
+<button bind:this={self} class="flex items-center px-2 py-1 btn btn--primary">
   <div class="p-2">
     <Icon path={mdiPlus} width="30px" height="30px" />
   </div>
   <div class="p-1">
-    {#if !active}
-      <div class="p-2">Tambahkan pertanyaan</div>
-    {:else}
-      <button
-        class="p-2 text-black bg-white"
-        on:click={(e) => emitQuestionType('pilihan')}>Pilihan</button>
-      <button
-        class="p-2 text-black bg-white"
-        on:click={(e) => emitQuestionType('teks')}>Teks</button>
+    <div class="p-2 inner-label">Tambahkan pertanyaan</div>
+    <div class="hidden-buttons">
+      <button class="p-2 text-black bg-white" on:click={() => emitQuestionType('selection')}>Pilihan</button>
+      <button class="p-2 text-black bg-white" on:click={() => emitQuestionType('essay')}>Teks</button>
       <button class="p-2 text-black bg-white">Lainnya</button>
-    {/if}
+    </div>
   </div>
 </button>
+
+<style>
+  .hidden-buttons {
+    display: none;
+  }
+
+  button:focus-within .inner-label {
+    display: none;
+  }
+
+  button:focus-within .hidden-buttons {
+    display: initial;
+  }
+</style>
