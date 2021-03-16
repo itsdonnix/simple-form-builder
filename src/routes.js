7a72f6d8324e2cc99fe588da9ef281ee8da663b2
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-16 22:18:34 +0800

    Update route
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,24 @@
 import { wrap } from 'svelte-spa-router/wrap';
+import * as utils from './utils';
+import { replace } from 'svelte-spa-router';
 
 const routes = {
   '/': wrap({
     asyncComponent: () => import('./views/index.svelte'),
   }),
-  '/form/new': wrap({
+
+  '/form/editor/new/': wrap({
+    asyncComponent: () => null,
+    conditions: [
+      async () => {
+        const hash = await utils.generateHash();
+        replace('/form/editor/' + hash);
+        return false;
+      },
+    ],
+  }),
+
+  '/form/editor/:id': wrap({
     asyncComponent: () => import('./views/form/form-editor.svelte'),
   }),
 };
--- src/views/index.svelte
+++ src/views/index.svelte
@@ -8,11 +8,7 @@
   <div class="mt-4">
     <h3 class="p-2 font-bold h3">Baru</h3>
     <div class="m-3">
-      <a
-        class="flex flex-col new-form-card--wrapper"
-        href="/form/new"
-        title="Buat formulir baru"
-        use:link>
+      <a class="flex flex-col new-form-card--wrapper" href="/form/editor/new" title="Buat formulir baru" use:link>
         <div class="flex items-center justify-center new-form-card">
           <Icon path={mdiPlusCircle} />
         </div>
