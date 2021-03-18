3e00cc261bcc54103524270344b35ee197e21360
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-19 06:46:38 +0800

    Add form url info and copy to clibboard feature
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/components/formSetupHeader.svelte
+++ src/components/formSetupHeader.svelte
@@ -1,5 +1,15 @@
 <script>
+  import { mdiContentCopy } from '@mdi/js';
+  import Icon from './Icon.svelte';
+
+  export let id = '';
   let loading = false;
+
+  $: formURL = location.host + '/#/form/' + id;
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(formURL);
+  }
 
   function send() {
     loading = true;
@@ -11,7 +21,17 @@
 
 <div class="p-2 bg-white form-actions">
   <div class="flex margin-page">
-    <button class="px-4 py-2 ml-auto font-bold btn btn--primary" on:click={send}>
+    <input
+      on:click={copyToClipboard}
+      class="px-4 py-2 border"
+      type="text"
+      class:hidden={!!!id}
+      value={formURL}
+      title={formURL} />
+    <button on:click={copyToClipboard} class="p-1" title="Salin alamat formulir">
+      <Icon path={mdiContentCopy} />
+    </button>
+    <button class="px-4 py-2 ml-auto font-bold btn btn--primary" on:click={send} title="Kirim formulir">
       {loading ? 'Mengirimkan...' : 'Kirim'}
     </button>
   </div>
