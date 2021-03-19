8c5d010f47c66424fe71f397f8e043a0122614f1
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-20 07:50:14 +0800

    Add navigation to home
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-  import Router, { location } from 'svelte-spa-router';
+  import Router, { location, link } from 'svelte-spa-router';
   import routes from './routes';
 </script>
 
 {#if $location.startsWith('/form/editor') || $location === '/'}
   <header class="text-white main-header margin-page">
-    <div class="app-name">Formulir</div>
+    <a class="app-name" href="/" use:link>Formulir</a>
   </header>
 {/if}
 
