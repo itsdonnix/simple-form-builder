b2ba35e0f94fce6c02c748aabb160434dc1f35e9
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-20 06:41:05 +0800

    Improve session storage handling
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/session.js
+++ src/session.js
@@ -2,10 +2,15 @@ import * as utils from './utils';
 
 export let sessionID = sessionStorage.getItem('randomId');
 
-(async () => {
-  !sessionID && sessionStorage.setItem('randomId', await utils.generateHash());
-})();
+generateSessionIdIfNotExist();
+
+function generateSessionIdIfNotExist() {
+  (async () => {
+    !sessionID && sessionStorage.setItem('randomId', await utils.generateHash());
+  })();
+}
 
 export function getSessionId() {
+  generateSessionIdIfNotExist();
   return sessionStorage.getItem('randomId');
 }
