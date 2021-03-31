09662dbce62c33b916f5b535f7f70651043dc4a4
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-04-01 06:11:54 +0800

    Using english instead
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/components/formSetupHeader.svelte
+++ src/components/formSetupHeader.svelte
@@ -29,7 +29,7 @@
       on:click={() => confirm('Are you sure want to delete this form?') && emit('delete-form-clicked')}
       title="Delete this form">
       <Icon path={mdiTrashCanOutline} />
-      <span class="mx-2">Hapus formulir</span>
+      <span class="mx-2">Delete Form</span>
     </button>
     <input
       on:click={copyToClipboard}
