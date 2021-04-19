f7d1ccbe4ac56dde617928f92dfdf59392626ad8
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-04-20 00:56:02 +0800

    Configure base dir & change build target to es2015
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ const { defineConfig } = require('vite');
 module.exports = defineConfig(({ /* command, */ mode }) => {
   const isProduction = mode === 'production';
   return {
+    base: './',
+    build: {
+      target: 'es2015',
+    },
     plugins: [
       svelte({
         hot: !isProduction,
