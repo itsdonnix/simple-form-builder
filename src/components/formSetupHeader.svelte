a72a2dc1d4698f1a96fc7f15d9a2754178c82949
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-05-13 13:53:12 +0800

    Disable delete form button caption for now
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/components/formSetupHeader.svelte
+++ src/components/formSetupHeader.svelte
@@ -29,7 +29,7 @@
       on:click={() => confirm('Are you sure want to delete this form?') && emit('delete-form-clicked')}
       title="Delete this form">
       <Icon path={mdiTrashCanOutline} />
-      <span class="hidden mx-2 sm:inline-block">Delete Form</span>
+      <!-- <span class="hidden mx-2 sm:inline-block">Delete Form</span> -->
     </button>
     <input
       on:click={copyToClipboard}
