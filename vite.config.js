c88eb3d1bc13af4251eef7e6f009504c4c6a62e2
Don Alfons Nisnoni <donnisnoni@outlook.com>
2022-08-20 16:52:34 +0800

    Update build target `es2016`
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig(({ /* command, */ mode }) => {
   return {
     base: './',
     build: {
-      target: 'es2015',
+      target: 'es2016',
     },
     plugins: [
       svelte({
