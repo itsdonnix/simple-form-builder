e268cb6e1719c7fb2ffc30416898e315565fb1e7
Don Alfons <donnisnoni@outlook.com>
2025-03-16 21:41:35 +0800

    fix: adjust class order for form editor layout
    - Swapped the order of classes in the form editor layout for better styling consistency.
    - Moved `w-full` class after `mx-5` for improved responsiveness and visual alignment.
    
    Signed-off-by: Don Alfons <donnisnoni@outlook.com>

--- src/views/form/form-editor.svelte
+++ src/views/form/form-editor.svelte
@@ -106,7 +106,7 @@
 <FormSetupHeader on:delete-form-clicked={removeForm} />
 
 <div class="flex flex-col items-center">
-  <div class="w-full mx-5 my-10 bg-transparent bg-white shadow-md" style="max-width: 700px">
+  <div class="mx-5 my-10 w-full bg-transparent bg-white shadow-md" style="max-width: 700px">
     <Tabs>
       <TabList>
         <Tab>Questions</Tab>
