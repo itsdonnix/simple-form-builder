1e3bc3db8ec501b2205a0ce05073dc2fe27bfa98
Don Alfons <donnisnoni@outlook.com>
2025-06-13 20:14:38 +0800

    feat: improve form answers component
    This commit enhances the `FormAnswers` component to provide a more user-friendly and informative display of form responses. It includes conditional rendering based on the presence of answers, clearer information in the heading, improved formatting for better readability, and correct handling of multiple-choice answers.
    
    Signed-off-by: Don Alfons <donnisnoni@outlook.com>

--- src/components/FormAnswers.svelte
+++ src/components/FormAnswers.svelte
@@ -1,31 +1,36 @@
 <script lang="ts">
   export const questions = [];
   export const respondents = [];
+
+  $: hasAnwers = respondents.length > 0;
+  $: info = respondents.length > 0 ? `Answers from (${respondents.length} respondents)` : 'No answers yet';
 </script>
 
 <div class="p-5" tabindex="0">
-  <h2 class="text-xl">
-    Answers from ({respondents.length} respondents)
+  <h2 class="text-xl" class:text-center={!hasAnwers} class:text-gray-500={!hasAnwers}>
+    {info}
   </h2>
-  <hr class="my-2" />
-  <div class="flex flex-col">
-    {#each respondents as respondent}
-      <details class="mb-3 ml-1">
-        <summary class="py-1 text-lg">{respondent.name} ({respondent.phoneNumber})</summary>
-        {#each respondent.answers as _answer, index}
-          <div class="flex p-1 font-bold">
-            <div class="mr-1">
-              {index + 1}.
+  {#if hasAnwers}
+    <hr class="my-2" />
+    <div class="flex flex-col">
+      {#each respondents as respondent}
+        <details class="mb-3 ml-1">
+          <summary class="py-1 text-lg">{respondent.name} ({respondent.phoneNumber})</summary>
+          {#each respondent.answers as _answer, index}
+            <div class="flex p-1 font-bold">
+              <div class="mr-1">
+                {index + 1}.
+              </div>
+              <div class="whitespace-pre">
+                {questions[index].text}
+              </div>
             </div>
-            <div class="whitespace-pre">
-              {questions[index].text}
+            <div class="p-1 ml-4 whitespace-pre">
+              {questions[index].multiple ? _answer.filter((_answer_) => !!_answer_).join(', ') : _answer}
             </div>
-          </div>
-          <div class="p-1 ml-4 whitespace-pre">
-            {questions[index].multiple ? _answer.filter((_answer_) => !!_answer_).join(', ') : _answer}
-          </div>
-        {/each}
-      </details>
-    {/each}
-  </div>
+          {/each}
+        </details>
+      {/each}
+    </div>
+  {/if}
 </div>
