b5290fe1bc0e1f5d67b5e5a6ba00746eb00e92ef
Don Alfons <donnisnoni@outlook.com>
2025-06-14 04:04:25 +0800

    style(index): adjust layout for recent forms section
    Update the layout of recent forms to use a flexible wrapping style,
    improving responsiveness and design consistency.
    
    Signed-off-by: Don Alfons <donnisnoni@outlook.com>

--- src/views/index.svelte
+++ src/views/index.svelte
@@ -26,7 +26,7 @@
   {#if $forms.length}
     <div class="mt-4">
       <h3 class="p-2 font-bold h3">Recent Forms</h3>
-      <div class="flex flex-col gap-2 m-3 md:flex-row">
+      <div class="flex flex-wrap gap-2 p-3">
         {#each $forms as form}
           <a class="flex relative flex-col w-full form-card" href="/form/edit/{form.id}" title={form.title} use:link>
             <div class="flex justify-center items-center new-form-card">
